454abe59a6c68bad097914b7fadc9f449be111c0
Andre1Karl <ramos.andreikarl.gmail.com>
2022-08-18 13:24:26 +0800

    weather js

---
+++ app.js
@@ -0,0 +1,55 @@
+const iconElement =document.querySelector('.weather-icon');
+const tempElement =document.querySelector('.temperature-value p');
+const descElement =document.querySelector('.temperature-description p');
+const locationElement =document.querySelector('.location p');
+const notificationElement =document.querySelector('.notification');
+const weather ={};
+weather.temperature = {
+  unit: 'celcius'
+};
+const KELVIN= 273;
+
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(getPosition, showError);
+} else {
+  notificationElement.style.display = 'block';
+  notificationElement.innerHTML = '<p> Browser Doesnt support geolocation';
+}
+
+function getPosition(position){
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  getWeather(latitude, longitude);
+  //console.log(latitude, longitude);
+}
+
+function showError(error){
+  notificationElement.style.display = 'block';
+  notificationElement.innerHTML = '<p> User Denied Geolocation';
+}
+
+function getWeather(latitude, longitude){
+  let api ='https://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=8dd59aab624365db200e32bdd70ce178'
+  //console.log(api);
+  fetch(api).then(function(response){
+    let data = response.json();
+    return data;
+  })
+  .then(function(data){
+    weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+    weather.description = data.weather[0].description;
+    weather.iconId = data.weather[0].icon;
+    weather.city = data.name;
+    weather.country = data.sys.country;
+  })
+  .then(function(){
+    displayWeather();
+  });
+}
+
+function displayWeather(){
+  iconElement.innerHTML = '<img src="icons/'+weather.iconId+'.png"/>';
+  tempElement.innerHTML = ''+weather.temperature.value+'°<span>C</span>';
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = ''+weather.city+', '+weather.country+'';
+}
